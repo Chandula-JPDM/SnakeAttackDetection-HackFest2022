1befdaa298118b4827945696af34494f5675035d
Chandula-JPDM <e16061@eng.pdn.ac.lk>
2022-05-08 00:15:24 +0530

    modify login resposnses

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from flask_login import login_user, login_required, logout_user
-from flask import Blueprint, url_for, redirect, request, flash
+from flask import Blueprint, url_for, redirect, request, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -13,17 +13,20 @@ def login():
     password = request.form.get('password')
 
     user = User.query.filter_by(email=email).first()
+    print(user)
     
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user:
-        if not (user.password == password):
-            flash('Please check your login details and try again.')
-            return 404 # if the user doesn't exist or password is wrong, reload the page
+    if not user or not (user.password == password):
+        # flash('Please check your login details and try again.')
+        response={
+            "message": 'Please check your login details and try again',
+        }
+        return (jsonify(response)) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user)
-    return redirect(url_for('main.profile'))
+    return (url_for('main.profile'))
 
 @auth.route('/logout')
 @login_required
